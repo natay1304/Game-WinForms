dfb461759c931a5cd756153670804a04dd504359
Anastasiya <natay1304@gmail.com>
2019-03-23 18:32:55 +0300

    Game Fly v1.0

---
+++ GameWF/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameWF
+{
+    public partial class Form1 : Form
+    {
+
+        int points = 0;
+        int maxPoint = 25;
+        int attemps = 3;
+        PictureBox[] boxes;
+        bool clicked = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            boxes = new PictureBox[5] {pictureBox1, pictureBox2, pictureBox3, pictureBox4, pictureBox5};
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label5.Text = points.ToString();
+            label3.Text = attemps.ToString();
+            label6.Text = maxPoint.ToString();
+            label7.Hide();
+            label8.Hide();
+            ShowNextBox(boxes);
+            timer1.Interval = 3000;
+            timer1.Enabled = true;
+        }
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            ClickOnBox(points, maxPoint, pictureBox1);
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            ClickOnBox(points, maxPoint, pictureBox2);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            ClickOnBox(points, maxPoint, pictureBox3);
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            ClickOnBox(points, maxPoint, pictureBox4);
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            ClickOnBox(points, maxPoint, pictureBox5);
+        }
+
+        private void ClickOnBox(int points, int maxPoint, PictureBox box)
+        {
+            clicked = true;
+            box.Hide();
+            if(points == maxPoint)
+            {
+                label7.Show();
+                HideAll(boxes);
+                timer1.Enabled = false;
+            }
+            else if(attemps <= 0)
+            {
+                label8.Show();
+                HideAll(boxes);
+                
+            }
+            points++;
+            UpdateScores();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (clicked)
+            {
+                points++;
+            }
+            else
+            {
+                attemps--;
+                if (attemps == 0)
+                {
+                    timer1.Enabled = false;
+                    HideAll(boxes);
+                }
+            }
+            clicked = false;
+
+            Random rand = new Random();
+            ShowNextBox(boxes);
+            boxes[rand.Next(5)].Show();
+            if(timer1.Interval > 900) timer1.Interval -= 250;
+            UpdateScores();
+        }
+
+        private void ShowNextBox(PictureBox[] boxes)
+        {
+            for(int i = 0; i < boxes.Length; i++)
+            {
+                boxes[i].Hide();
+            }
+            Random r = new Random();
+            boxes[r.Next(5)].Show();
+        }
+
+        private void HideAll(PictureBox[] boxes)
+        {
+            for (int i = 0; i < boxes.Length; i++)
+            {
+                boxes[i].Hide();
+            }
+        }
+
+        private void UpdateScores()
+        {
+            label5.Text = points.ToString();
+            label3.Text = attemps.ToString();
+            label6.Text = maxPoint.ToString();
+            Invalidate();
+        }
+    }
+}
---
+++ GameWF/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("GameWF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GameWF")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("fcc12e79-84cc-4f0a-9ed0-97c3c9b6e753")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
